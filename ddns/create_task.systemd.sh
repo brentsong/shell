44b99ac00bb7c0d153e522f9b7e3762e7b7d3085
brentsong <73120634+brentsong@users.noreply.github.com>
2021-07-08 18:07:23 +0800

    Update create_task.systemd.sh

--- ddns/create_task.systemd.sh
+++ ddns/create_task.systemd.sh
@@ -3,9 +3,9 @@ RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 CMD="/usr/local/bin/ddns -c /etc/config.json";
 
-# Add newfuture_ddns.timer to /etc/systemd/system
+# Add ddns.timer to /etc/systemd/system
 echo "[Unit]
-Description=NewFuture DDNS Timer
+Description=DDNS Timer
 Wants=network-online.target
 After=network-online.target
 
@@ -14,11 +14,11 @@ OnStartupSec=60
 OnUnitActiveSec=10
 
 [Install]
-WantedBy=timers.target" > /etc/systemd/system/newfuture_ddns.timer;
+WantedBy=timers.target" > /etc/systemd/system/ddns.timer;
 
-# Add newfuture_ddns.service to /etc/systemd/system
+# Add ddns.service to /etc/systemd/system
 echo "[Unit]
-Description=NewFuture DDNS Service
+Description=DDNS Service
 Wants=network-online.target
 After=network-online.target
 
@@ -29,11 +29,11 @@ ExecStart=$CMD
 TimeoutSec=180
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/newfuture_ddns.service;
+WantedBy=multi-user.target" > /etc/systemd/system/ddns.service;
 
-# Enable and start newfuture_ddns.timer
-systemctl enable newfuture_ddns.timer;
-systemctl start newfuture_ddns.timer;
+# Enable and start ddns.timer
+systemctl enable ddns.timer;
+systemctl start ddns.timer;
 
-echo "Use \"systemctl status newfuture_ddns.service\" to view run logs,
-Use \"systemctl status newfuture_ddns.timer\" to view timer logs."
+echo "Use \"systemctl status ddns.service\" to view run logs,
+Use \"systemctl status ddns.timer\" to view timer logs."
