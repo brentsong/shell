0c42de50df3b69bd8a61a91a130e1ba53a682c9e
brentsong <73120634+brentsong@users.noreply.github.com>
2021-08-06 18:42:40 +0800

    Create create_task.systemd.centos7.sh

---
+++ ddns/create_task.systemd.centos7.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+CMD="/usr/bin/ddns -c /etc/config.json";
+
+# Add ddns.timer to /etc/systemd/system
+echo "[Unit]
+Description=DDNS Timer
+Wants=network-online.target
+After=network-online.target
+[Timer]
+OnStartupSec=60
+OnUnitActiveSec=10
+[Install]
+WantedBy=timers.target" > /etc/systemd/system/ddns.timer;
+
+# Add ddns.service to /etc/systemd/system
+echo "[Unit]
+Description=DDNS Service
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=root
+Type=oneshot
+ExecStart=$CMD
+TimeoutSec=180
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/ddns.service;
+
+# Enable and start ddns.timer
+systemctl enable ddns.timer;
+systemctl start ddns.timer;
+
+echo "Use \"systemctl status ddns.service\" to view run logs,
+Use \"systemctl status ddns.timer\" to view timer logs."
